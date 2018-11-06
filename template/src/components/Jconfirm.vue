a7aaa048aa81faa2c1b4e6c74bac276e71548e69
zengping <331962716@qq.com>
2018-11-06 13:57:32 +0800

    update

--- template/src/components/Jconfirm.vue
+++ template/src/components/Jconfirm.vue
@@ -1,18 +1,28 @@
 <script>
 export default {
   template: '<div/>',
+  data () {
+    return {
+      status: false
+    }
+  },
   mounted () {
     this.$bus.$on('confirm', this.init)
   },
   methods: {
     init (o) {
+      if (this.status) return
+      this.status = true
+      let self = this
       this.$confirm(o.text, o.title ? o.title : '提醒', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
-      }).then((res) => {
+      }).then(res => {
+        self.status = false
         if (o.callback) o.callback()
-      }).catch(() => {
+      }).catch(err => {
+        self.status = false
       })
     }
   }
